e4410a214dcf790a15cc186fcbc9cc1af9d39e7d
joshuaskipper <yasoiskipper1@outlook.com>
2026-08-06 10:34:45 -0400

    Added models, interface, repository, and endpoints

--- DailyScriptureApi/EndPoints/VerseEndPoint.cs
+++ DailyScriptureApi/EndPoints/VerseEndPoint.cs
@@ -1,10 +1,28 @@
-﻿namespace DailyScriptureApi.EndPoints
+﻿using DailyScriptureApi.Services.Interface;
+
+namespace DailyScriptureApi.EndPoints
 {
     public static class VerseEndPoint
     {
         public static void MapVerseEndPoint(this IEndpointRouteBuilder app) 
         {
-            
+            app.MapGet("/api/verse", async (IVerseRepository verseRepository) =>
+            {
+                var verseDomain = await verseRepository.GetAllAsync();
+
+                return Results.Ok(verseDomain);
+            });
+
+            app.MapGet("/api/verse/{id:int}", async (IVerseRepository verseRepository, int id) =>
+            {
+                var verseDomain = await verseRepository.GetById(id);
+
+                if (verseDomain is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(verseDomain);
+            });
         }
     }
 }
