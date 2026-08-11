399b0e98132bb7e733d3ff9790ea83639699537a
joshuaskipper <yasoiskipper1@outlook.com>
2026-08-11 15:09:33 -0400

    added sorting filter and pagination

--- DailyScriptureApi/Services/Interface/IVerseRepository.cs
+++ DailyScriptureApi/Services/Interface/IVerseRepository.cs
@@ -4,7 +4,8 @@ namespace DailyScriptureApi.Services.Interface
 {
     public interface IVerseRepository
     {
-        public Task<List<Verse>> GetAllAsync();
+        public Task<List<Verse>> GetAllAsync(string? filterOn, string? filterQuery, string? sortOn, bool? isAscending,
+            int pageNumber = 1, int pageSize = 20);
         public Task<Verse> GetById(int id);
     }
 }
